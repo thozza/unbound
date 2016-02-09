175bd4427de1f5362ab006821277ce20bc2fc7ef
Tomas Hozza <thozza@redhat.com>
2016-02-09 16:47:41 +0100

    Regenerate config lexer using bison
    echo "#include \"config.h\"" > util/configlexer.c || echo "Failed to create configlexer"
    echo "#include \"util/configyyrename.h\"" >> util/configlexer.c || error_cleanup "Failed to create configlexer"
    flex -i -t util/configlexer.lex >> util/configlexer.c  || echo "Failed to create configlexer"
    if test -x `which bison` 2>&1; then YACC=bison; else YACC=yacc; fi
    $YACC -y -d -o util/configparser.c util/configparser.y || echo "Failed to create configparser"
    
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- util/configparser.h
+++ util/configparser.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -207,7 +207,8 @@ extern int yydebug;
     VAR_CAPS_WHITELIST = 417,
     VAR_CACHE_MAX_NEGATIVE_TTL = 418,
     VAR_PERMIT_SMALL_HOLDDOWN = 419,
-    VAR_QNAME_MINIMISATION = 420
+    VAR_QNAME_MINIMISATION = 420,
+    VAR_MIXED_MODE_FWD = 421
   };
 #endif
 /* Tokens.  */
@@ -374,20 +375,19 @@ extern int yydebug;
 #define VAR_CACHE_MAX_NEGATIVE_TTL 418
 #define VAR_PERMIT_SMALL_HOLDDOWN 419
 #define VAR_QNAME_MINIMISATION 420
+#define VAR_MIXED_MODE_FWD 421
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-
+typedef union YYSTYPE YYSTYPE;
 union YYSTYPE
 {
-#line 64 "./util/configparser.y" /* yacc.c:1909  */
+#line 64 "util/configparser.y" /* yacc.c:1909  */
 
 	char*	str;
 
-#line 388 "util/configparser.h" /* yacc.c:1909  */
+#line 390 "util/configparser.h" /* yacc.c:1909  */
 };
-
-typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
 #endif
